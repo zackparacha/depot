07358e9211b26b364d1cefafe60140c00637cb6d
zack paracha <zackparacha@gmail.com>
2024-02-09 13:54:21 +0500

    add rescure for record not found for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts or /carts.json
   def index
@@ -58,6 +59,11 @@ class CartsController < ApplicationController
   end
 
   private
+    # Rescue exceptions if cart is not found
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
