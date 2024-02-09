2a2dda4c9f04d855fd311960978daa1781153512
zack paracha <zackparacha@gmail.com>
2024-02-09 18:24:08 +0500

    update validation for image

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,13 @@ class Product < ApplicationRecord
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
     validates :title, uniqueness: true
+
     validates :image_url, allow_blank: true, format: {
         with:
-        %r{\.(gif|jpg|png)\z}i,
-        message: "must be a URL for GIF, JPG or PNG image."
+        /\A.*\.(gif|jpg|png)\z/i,
+        message: "must be a URL for GIF, JPG, or PNG image."
     }
+
 
     has_many :line_items
     before_destroy :ensure_not_referenced_by_any_line_item
