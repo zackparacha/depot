c1473f15060197973821bf9a4ed036a3805cbe2b
zack paracha <zackparacha@gmail.com>
2024-03-17 17:26:04 +0500

    Limiting Access

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  skip_before_action :authorize, only: %i[ new create ]
   include CurrentCart
   before_action :set_cart, only: %i[ new create ]
   before_action :ensure_cart_isnt_empty, only: %i[ new ]
